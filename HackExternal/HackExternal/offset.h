39c7d7c5dfd7a39d90acc323fe0883aa2f05320c
blackG43 <105086242+blackG43@users.noreply.github.com>
2022-05-06 18:33:02 -0500

    Update offset.h

--- HackExternal/HackExternal/offset.h
+++ HackExternal/HackExternal/offset.h
@@ -3,19 +3,129 @@
 
 #ifndef BLOODHUNT_H
 
+
 #include <windows.h>
 #include <psapi.h>
 #include <tlhelp32.h>
 #include <iostream>
 #include <dwmapi.h>
-#include <d3d9.h>
-#include <d3dx9.h>
+#include  <d3d9.h>
+#include  <d3dx9.h>
 
 #include "singleton.h"
-#include "vector.h"
+
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "dwmapi.lib")
 
+
+
+#define M_PI 3.14159265358979323846264338327950288419716939937510
+
+
+
+class Vector3
+{
+public:
+	Vector3() : x(0.f), y(0.f), z(0.f)
+	{
+
+	}
+
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	~Vector3()
+	{
+
+	}
+
+	float x;
+	float y;
+	float z;
+
+	inline float Dot(Vector3 v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	inline float Distance(Vector3 v)
+	{
+		return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
+	}
+
+	Vector3 operator+(Vector3 v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(Vector3 v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(float number) const {
+		return Vector3(x * number, y * number, z * number);
+	}
+};
+
+struct FQuat
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+struct FTransform
+{
+	FQuat rot;
+	Vector3 translation;
+	char pad[4];
+	Vector3 scale;
+	char pad1[4];
+
+	D3DMATRIX ToMatrixWithScale()
+	{
+		D3DMATRIX m;
+		m._41 = translation.x;
+		m._42 = translation.y;
+		m._43 = translation.z;
+
+		float x2 = rot.x + rot.x;
+		float y2 = rot.y + rot.y;
+		float z2 = rot.z + rot.z;
+
+		float xx2 = rot.x * x2;
+		float yy2 = rot.y * y2;
+		float zz2 = rot.z * z2;
+		m._11 = (1.0f - (yy2 + zz2)) * scale.x;
+		m._22 = (1.0f - (xx2 + zz2)) * scale.y;
+		m._33 = (1.0f - (xx2 + yy2)) * scale.z;
+
+		float yz2 = rot.y * z2;
+		float wx2 = rot.w * x2;
+		m._32 = (yz2 - wx2) * scale.z;
+		m._23 = (yz2 + wx2) * scale.y;
+
+		float xy2 = rot.x * y2;
+		float wz2 = rot.w * z2;
+		m._21 = (xy2 - wz2) * scale.y;
+		m._12 = (xy2 + wz2) * scale.x;
+
+		float xz2 = rot.x * z2;
+		float wy2 = rot.w * y2;
+		m._31 = (xz2 + wy2) * scale.z;
+		m._13 = (xz2 - wy2) * scale.x;
+
+		m._14 = 0.0f;
+		m._24 = 0.0f;
+		m._34 = 0.0f;
+		m._44 = 1.0f;
+
+		return m;
+	}
+};
 
 inline namespace BloodHunt
 {
@@ -27,7 +137,7 @@ inline namespace BloodHunt
 		uint64_t dwProcess_Base = NULL;
 		HWND gameHWND = NULL;
 
-		int actor_count = NULL;
+		int Local_PlayerID = NULL;
 		int ScreenHeight = NULL;
 		int ScreenWidth = NULL;
 		int ScreenLeft = NULL;
@@ -48,9 +158,8 @@ inline namespace BloodHunt
 		DWORD_PTR local_player_state = NULL;
 		DWORD_PTR persistent_level = NULL;
 		DWORD_PTR actors = NULL;
-		DWORD_PTR ranged_weapon_component = NULL;
-		DWORD_PTR equipped_weapon_type = NULL;
-
+		int actor_count = NULL;
+		int local_player_id = NULL;
 
 	};
 #define GameVars BloodHunt::Variables::Get()
@@ -58,35 +167,21 @@ inline namespace BloodHunt
 	class Offsets : public Singleton<Offsets>
 	{
 	public:
-
-		DWORD offset_u_world = 0x55fcbc0;
-		DWORD offset_g_names = 0x54aa8c0;
-		DWORD offset_camera_manager = 0x2c0; // APlayerController->PlayerCameraManager
-		DWORD offset_camera_cache = 0x1ab0; //APlayerCameraManager->CameraCachePrivate
-		DWORD offset_persistent_level = 0x30; //UWorld->PersistentLevel
-		DWORD offset_game_instance = 0x180; //UWolrd->OwningGameInstance
-		DWORD offset_local_players_array = 0x38; //UGameInstance->LocalPlayers
-		DWORD offset_player_controller = 0x30; //UPlayer->PlayerController
-		DWORD offset_apawn = 0x2a8;  //APlayerController->AcknowledgedPawn
-		DWORD offset_root_component = 0x138; //AActor->RootComponent
-		DWORD offset_actor_array = 0x98; //UNetConnection->OwningActor
-		DWORD offset_actor_count = 0xa0; //UNetConnection->MaxPacket
-		DWORD offset_actor_id = 0x18;
-		DWORD offset_player_name = 0x308;
-		DWORD offset_player_state = 0x248; //APawn->PlayerState
-		DWORD offset_actor_mesh = 0x288; //ACharacter->Mesh
-		DWORD offset_bone_array = 0x4B0;
-		DWORD offset_component_to_world = 0x1c0;
-		DWORD offset_ranged_weapon_component = 0x4c8;
-		DWORD offset_equipped_weapon_type = 0x170;
-		DWORD offset_relative_location = 0x11c; //USceneComponent->RelativeLocation
-		DWORD offset_last_submit_time = 0x288;
-		DWORD offset_last_render_time = 0x28C;
-		DWORD offset_health = 0x668;
-		DWORD offset_max_health = 0x56C;
+		DWORD offset_uworld = 0x5741680;
+		DWORD offset_camera_manager = 0x2c0;
+		DWORD offset_camera_cache = 0x1ab0;  //0x2a0  0x1ab0
 	};
 
 #define GameOffset BloodHunt::Offsets::Get()
+
+	D3DMATRIX MatrixMultiplication(D3DMATRIX pM1, D3DMATRIX pM2);
+	FTransform GetBoneIndex(DWORD_PTR mesh, int index);
+	Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id);
+	D3DXMATRIX Matrix(Vector3 rot, Vector3 origin = Vector3(0, 0, 0));
+	Vector3 ProjectWorldToScreen(Vector3 WorldLocation);
+	std::string GetNameFromFName(int key);
 }
+
+
 #endif  !BLOODHUNT_H
 
